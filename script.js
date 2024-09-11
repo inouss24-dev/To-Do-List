083d389b65fac9e4a2214bbde027f62a26cdbb8c
inouss24-dev <mrinouss24@gmail.com>
2024-09-11 22:57:13 +0000

    Indenter

--- script.js
+++ script.js
@@ -1,30 +1,22 @@
 // Recuperation de données ....
-
 let input   = document.querySelector('input');
 let btn     = document.getElementById('btn');
 let error   = document.getElementById('error');
 let succes  = document.getElementById('succes');
 let list    = document.getElementById('list');
-
 //Declaration Des Variables
 let content;
 let listDiv
 error.style.display = 'none';
 succes.style.display = 'none';
-
 // Fonction pour l'affichage des Todo List dans le container 
-
-
 function add(e){
     listDiv = document.createElement('div');
     listDiv.textContent = e;
     listDiv.className = 'list listBackground';
     document.getElementById('list').append(listDiv);
 }
-
 // Les diiferent évenement 
-
-
 btn.addEventListener('click', () => {
     if(input.value == ""){
         input.style.border= "solid red";
