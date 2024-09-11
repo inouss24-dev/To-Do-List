a867829f97a323d38f66c8eb3c600481ad92c439
inouss24-dev <mrinouss24@gmail.com>
2024-09-11 22:45:48 +0000

    Changer le backgroud et divers

--- script.js
+++ script.js
@@ -5,19 +5,25 @@ let btn     = document.getElementById('btn');
 let error   = document.getElementById('error');
 let succes  = document.getElementById('succes');
 let list    = document.getElementById('list');
-let listDiv
 
 //Declaration Des Variables
 let content;
-
+let listDiv
 error.style.display = 'none';
 succes.style.display = 'none';
+
+// Fonction pour l'affichage des Todo List dans le container 
+
+
 function add(e){
     listDiv = document.createElement('div');
     listDiv.textContent = e;
     listDiv.className = 'list listBackground';
     document.getElementById('list').append(listDiv);
 }
+
+// Les diiferent évenement 
+
 
 btn.addEventListener('click', () => {
     if(input.value == ""){
@@ -39,4 +45,5 @@ btn.addEventListener('click', () => {
         add(content)
     }
 })
+
 
